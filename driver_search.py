89ef84588775cd55515907a7d2207e6e64d12bb7
Carl Sobolewski <sobolews@ualberta.ca>
2014-03-14 10:09:24 -0600

    Update driver_search; first search working but needs formatting

--- driver_search.py
+++ driver_search.py
@@ -13,12 +13,21 @@ class DriverSearch(npyscreen.FormBaseNew):
         # will be [].
 
         self.user_query = self.add(npyscreen.TitleText, name="Enter Search Query:",
-                                   begin_entry_at=21, use_two_lines=False)
-        self.query_confirm = self.add(npyscreen.ButtonPress, name="Search")
+                                   begin_entry_at=21, use_two_lines=False, field_width=35)
+
+        self.query_confirm = self.add(npyscreen.ButtonPress, name="Search", rely=5, relx=60)
         self.query_confirm.whenPressed = self.process_query
+
+        self.results = self.add(npyscreen.Pager, name="Results:", height=2,
+                                max_height=10, scroll_exit=True,
+                                slow_scroll=True, exit_left=True, exit_right=True)
 
         self.backbutton = self.add(npyscreen.ButtonPress, name="Back", rely=21, relx=68)
         self.backbutton.whenPressed = lambda: self.parentApp.switchForm("SEARCHENGINE")
+        
+
+        
+
 
 
     def process_query(self):
@@ -26,6 +35,12 @@ class DriverSearch(npyscreen.FormBaseNew):
             npyscreen.notify_confirm("Please select a search type.", editw=1,
                                      title='Error')
         elif self.chooser.value[0] == 0:
+            query = """SELECT * FROM people WHERE name = :q_name"""
+            self.results.values = self.parentApp.db.query({"q_name":self.user_query.value}, query)
+            npyscreen.notify_confirm(str(self.results.values), editw=1,
+                                     title='Results')
+            
+
             pass # do name search
         elif self.chooser.value[0] == 1:
             pass # do licence number search
