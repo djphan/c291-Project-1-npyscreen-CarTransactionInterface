0196ddb967560f07880f1b9a66b82219ce3395a6
jcairo <joncairo@gmail.com>
2014-03-15 11:54:33 -0600

    Added more form checking for owner registration

--- new_vehicle_registration.py
+++ new_vehicle_registration.py
@@ -2,7 +2,6 @@ import npyscreen
 
 class NewVehicleRegistration(npyscreen.ActionForm):
     def create(self):
-
         self.serial_no = self.add(npyscreen.TitleText, name='Serial no:')
         self.maker = self.add(npyscreen.TitleText, name='Maker:')
         self.model = self.add(npyscreen.TitleText, name='Model:')
@@ -13,8 +12,14 @@ class NewVehicleRegistration(npyscreen.ActionForm):
         self.button1 = self.add(npyscreen.ButtonPress, name = "Add owner")
         self.button1.whenPressed = self.button_press_add_owner
         # self.owner = self.add(npyscreen.MultiLineEdit, name=':')
+        # self.parentApp.serial_no = None
 
     def button_press_add_owner(self):
+        # check if serial has been filled so we can
+        # auto fill it in the subform
+        # if self.serial_no.value:
+            # self.parentApp.serial_no = self.serial_no.value
+        
         self.parentApp.switchForm("ADDOWNERONVEHICLE")
 
     def on_ok(self):
