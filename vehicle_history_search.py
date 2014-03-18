091c28f12f0a418a57d6bf231c252171885e5906
Daniel Phan <djphan@cs.ualberta.ca>
2014-03-18 16:14:39 -0600

    Table formatting

--- vehicle_history_search.py
+++ vehicle_history_search.py
@@ -30,7 +30,7 @@ class VehicleHistorySearch(npyscreen.ActionFormCarl):
     
         # build query for when user picks "Search by vehicle serial no"
         query = """ 
-            SELECT  h.serial_no, count(DISTINCT transaction_id), avg(price), count(DISTINCT t.ticket_no)
+            SELECT  h.serial_no AS Serial_No, count(DISTINCT transaction_id) AS Number_of_Sales, avg(price) AS Average_Price, count(DISTINCT t.ticket_no) AS Number_of_Tickets 
             FROM   	vehicle h, auto_sale a, ticket t
             WHERE   t.vehicle_id (+)= h.serial_no AND
                     a.vehicle_id (+)= h.serial_no AND
@@ -55,20 +55,6 @@ class VehicleHistorySearch(npyscreen.ActionFormCarl):
         self.results.values = ['\n']
         joined = dict()
          
-        #for line in results:
-        #    if not line[4] in joined:
-        #        joined[line[4]] = list()
-        #        joined[line[4]].append("Vehicle Serial No: %s\n"%line[0])                              
-        #        joined[line[4]].append("Number of Transactions: %s\n"%(line[1], "N/A")[not line[1]])
-        #        joined[line[4]].append("Average Selling Price: %s\n"%(line[2], "N/A")[not line[2]])
-        #        joined[line[4]].append("Number of Tickets on Record: %s\n"%((line[3], "N/A")[not line[3]]))
-        #        joined[line[4]].append('\n')
-
-        #for record in joined:
-        #    self.results.values.extend(joined[record])
-        #npyscreen.notify_confirm(str(len(joined))+" result"+('s','')[len(joined)==1]+" found.", editw=1, title='')
-
-
         # get column names for printing 
         column_name = self.parentApp.db.cursor.description
        
@@ -82,10 +68,10 @@ class VehicleHistorySearch(npyscreen.ActionFormCarl):
                     # format column name
                     c_name = column_name[column_no][0].replace("_", " ").lower().capitalize()+':'
                     joined[record_no].append(
-                        "{0: <15}{1}\n".format(c_name, str(record[column_no])))
+                        "{0: <20} {1}\n".format(c_name, str(record[column_no])))
                 else:
                     c_name = column_name[column_no][0].replace("_", " ").lower().capitalize()+':'
-                    joined[record_no].append("{0: <15}{1}\n".format(c_name, "N/A"))
+                    joined[record_no].append("{0: <20}{1}\n".format(c_name, "N/A"))
 
             joined[record_no].append('\n')
             # append the result to the form
