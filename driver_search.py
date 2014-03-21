c4ec11a2d4eeec2c157a5ae22c116b64a5b330bf
Carl Sobolewski <sobolews@ualberta.ca>
2014-03-21 15:51:49 -0600

    commenting/formatting

--- driver_search.py
+++ driver_search.py
@@ -1,9 +1,7 @@
 import npyscreen
-import curses
 from npyscreen import wgwidget as widget
 
 class DriverSearch(npyscreen.FormBaseNew):
-
     # to have ESC --> go back:
     def set_up_exit_condition_handlers(self):
         super().set_up_exit_condition_handlers()
@@ -11,32 +9,28 @@ class DriverSearch(npyscreen.FormBaseNew):
             widget.EXITED_ESCAPE:   self.on_ok
         })
 
-
     def create(self):
+        self.backbutton = self.add(npyscreen.ButtonPress, name="Back")
+        self.backbutton.whenPressed = self.on_ok
+        self.nextrely+=1
         self.chooser = self.add(npyscreen.SelectOne, max_height=2,
                                 scroll_exit=True)
         self.chooser.values = ["Search by name", "Search by licence number"]
-
         self.nextrely+=1
-        # NOTE: When an entry is selected, the value of self.chooser.value will
-        # be a list containing the index of the entry in the above list - in
-        # this example, [0] or [1]. If nothing is selected, self.chooser.value
-        # will be [].
-
         self.user_query = self.add(npyscreen.TitleText, name="Search Query:",
-                                   begin_entry_at=15, use_two_lines=False, field_width=54)
+                                   begin_entry_at=15, use_two_lines=False,
+                                   field_width=54)
         self.nextrely-=1
         self.query_confirm = self.add(npyscreen.ButtonPress, name="OK", relx=70)
         self.query_confirm.whenPressed = self.process_query
 
         self.results = self.add(npyscreen.Pager, name="Results:", height=16,
-                                max_height=16, scroll_exit=True,
-                                slow_scroll=True, exit_left=True, exit_right=True)
+                                max_height=14, scroll_exit=True,
+                                slow_scroll=True, exit_left=True,
+                                exit_right=True)
 
-
-        
     def on_ok(self):
-        self.parentApp.switchForm("SEARCHENGINE")        
+        self.parentApp.switchForm("SEARCHENGINE")
 
     def process_query(self):
         if not self.chooser.value:
@@ -45,13 +39,13 @@ class DriverSearch(npyscreen.FormBaseNew):
             return
 
         if not self.user_query.value:
-            npyscreen.notify_confirm("Please enter a Name or Licence Number.", 
+            npyscreen.notify_confirm("Please enter a Name or Licence Number.",
                 editw=1, title='Error')
             return
 
         if self.chooser.value[0] == 0:
             query = """
-SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, c.c_id, 
+SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, c.c_id,
         c.description, l.expiring_date, p.sin
 FROM people p, drive_licence l, driving_condition c, restriction r
 WHERE p.sin = l.sin(+) AND
@@ -59,11 +53,12 @@ WHERE p.sin = l.sin(+) AND
       r.r_id = c.c_id(+) AND
       UPPER(p.name) = UPPER(:q_name)
             """
-            results = self.parentApp.db.query({"q_name":self.user_query.value}, query)
+            results = self.parentApp.db.query({"q_name":self.user_query.value},
+                                              query)
 
         elif self.chooser.value[0] == 1:
             query = """
-SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, c.c_id, 
+SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, c.c_id,
         c.description, l.expiring_date, p.sin
 FROM people p, drive_licence l, driving_condition c, restriction r
 WHERE p.sin = l.sin(+) AND
@@ -71,11 +66,14 @@ WHERE p.sin = l.sin(+) AND
       r.r_id = c.c_id(+) AND
       UPPER(l.licence_no) = UPPER(:licence_no)
             """
-            results = self.parentApp.db.query({"licence_no":self.user_query.value.ljust(15, ' ')}, query)
+            results = self.parentApp.db.query(
+                {"licence_no":self.user_query.value.ljust(15, ' ')}, query)
 
-        # if we get an empty list as a query result notify the user
+        # If we get an empty list as a query result notify the user
         if not results:
-            npyscreen.notify_confirm("No results were found for your query. Either Name or Licence Number does not exist in database.", 
+            npyscreen.notify_confirm(
+                "No results were found for your query. "
+                "Either Name or Licence Number does not exist in database.",
                 editw=1, title='Error')
             return
 
@@ -84,24 +82,29 @@ WHERE p.sin = l.sin(+) AND
         joined = dict()
         for line in results:
             if not line[8] in joined:
+                # Format output nicely:
                 joined[line[8]] = list()
-                joined[line[8]].append("Name:          %s\n"%line[0])                              
-                joined[line[8]].append("Licence No:    %s\n"%(line[1], "N/A")[not line[1]])
-                joined[line[8]].append("Address:       %s\n"%(line[2], "N/A")[not line[2]])
-                if line[3]: joined[line[8]].append("Birthday:      %s\n"%line[3].strftime("%d-%b-%y").upper())
+                joined[line[8]].append("Name:          %s\n"%line[0])
+                joined[line[8]].append("Licence No:    %s\n"%(line[1], "N/A")
+                                       [not line[1]])
+                joined[line[8]].append("Address:       %s\n"%(line[2], "N/A")
+                                       [not line[2]])
+                if line[3]: joined[line[8]].append("Birthday:      %s\n"%(
+                        line[3].strftime("%d-%b-%y").upper()))
                 else:       joined[line[8]].append("Birthday:      N/A\n")
-                joined[line[8]].append("Licence Class: %s\n"%((line[4], "N/A")[not line[4]]))
-                joined[line[8]].append("Conditions:    %s\n"%(("%s - %s"%(line[5], line[6]), "N/A")[not line[5]]))
-                if line[7]: joined[line[8]].append("Expiring Date: %s\n"%line[7].strftime("%d-%b-%y").upper())
+                joined[line[8]].append("Licence Class: %s\n"%((line[4], "N/A")
+                                                              [not line[4]]))
+                joined[line[8]].append("Conditions:    %s\n"%(("%s - %s"%(
+                                line[5], line[6]), "N/A")[not line[5]]))
+                if line[7]: joined[line[8]].append("Expiring Date: %s\n"%(
+                        line[7].strftime("%d-%b-%y").upper()))
                 else:       joined[line[8]].append("Expiring Date: N/A\n")
-                joined[line[8]].append('\n')                                                       
+                joined[line[8]].append('\n')
             else:
-                joined[line[8]].insert(-2, ' '*15+'%s\n'%('%s - %s'%(line[5], line[6]), "N/A")[not line[5]])
+                joined[line[8]].insert(-2, ' '*15+'%s\n'%('%s - %s'%(
+                            line[5], line[6]), "N/A")[not line[5]])
 
         for person in joined:
             self.results.values.extend(joined[person])
-        npyscreen.notify_confirm(str(len(joined))+" result"+('s','')[len(joined)==1]+" found.", editw=1, title='')
-
-        
-
-
+        npyscreen.notify_confirm(str(len(joined))+" result"+('s','')
+                                 [len(joined)==1]+" found.", editw=1, title='')
--- new_vehicle_registration.py
+++ new_vehicle_registration.py
@@ -2,7 +2,7 @@ import npyscreen
 
 class NewVehicleRegistration(npyscreen.ActionForm):
     def create(self):
-        # setup all fields to hold user input, label data, and buttons
+        # Setup all fields to hold user input, label data, and buttons
         self.nextrelx+=1
         self.serial_no = self.add(npyscreen.TitleText, name='Serial No:')
         self.maker = self.add(npyscreen.TitleText, name='Maker:')
@@ -14,7 +14,6 @@ class NewVehicleRegistration(npyscreen.ActionForm):
         self.button1 = self.add(npyscreen.ButtonPress, name = "Add owner")
         self.button1.whenPressed = self.button_press_add_owner
         self.nextrelx+=2
-
         self.add(npyscreen.TitleFixedText, name="Primary owner:", 
             editable=False, max_width=16, width=16)
         self.nextrely-=2; self.nextrelx+=20
@@ -33,39 +32,37 @@ class NewVehicleRegistration(npyscreen.ActionForm):
         self.o_owners.values = self.parentApp.NVR_other_owners
         self.parentApp.NVR_cancelled_entry = None
 
-    # setup button press method to be called when add owner buttons is pressed
+    # Setup button press method to be called when add owner buttons is pressed.
     def button_press_add_owner(self):
-        # attach attribute to main application for subform accesss
+        # Attach attribute to main application for subform accesss.
         self.parentApp.AOOV_default = self.serial_no.value
         self.parentApp.switchForm("ADDOWNERONVEHICLE")
     
-    # setup process methof to be used to create SQL statement dictionary
+    # Setup process method to be used to create SQL statement dictionary.
     def process_data(self):
-        # For year and type_id cast to int if possible
+        # For year and type_id cast to int if possible.
         self.values = {'serial_no': self.serial_no.value,
                        'maker': self.maker.value,
                        'model': self.model.value,
-                       'year': '' if self.year.value == '' else int(self.year.value),
+                       'year': '' if self.year.value == ''
+                                  else int(self.year.value),
                        'color': self.color.value,
-                       'type_id' : '' if self.type_id.value == '' else int(self.type_id.value)
+                       'type_id' : '' if self.type_id.value == ''
+                                      else int(self.type_id.value)
                       }
         return self.values
     
-    # run to create the prepare statement
+    # Run to create the prepare statement
     def prepare_statement(self):
-        return """insert into vehicle values (:serial_no,
-                                              :maker,
-                                              :model,
-                                              :year,
-                                              :color,
-                                              :type_id) """
+        return """insert into vehicle values (:serial_no, :maker, :model, :year,
+                                              :color, :type_id) """
     
-    # this method is run when the user has populated form and 
-    # wishes to send the data to the db
+    # This method is run when the user has populated form and wishes to send the
+    # data to the db.
     def validate_entries(self):
-        # ensure we can convert the user data to the appropriate data types
+        # Ensure we can convert the user data to the appropriate data types
         # check type_id and year separately, but not if they're left
-        # blank (to allow NULL values)
+        # blank (to allow NULL values).
         try:
             if not self.year.value == '':
                 int(self.year.value)
@@ -80,92 +77,108 @@ class NewVehicleRegistration(npyscreen.ActionForm):
                 int(self.type_id.value)
         except ValueError:
             npyscreen.notify_confirm("Type ID must be an integer.",
-                                     title="Error", form_color='STANDOUT', wrap=True, 
+                                     title="Error", form_color='STANDOUT',
+                                     wrap=True, wide=False, editw=1)
+            return False
+
+        # Check if serial_no is in existing database
+        query = """SELECT count(SERIAL_NO) FROM vehicle where SERIAL_NO =
+                   :serial_no"""
+        if self.parentApp.db.query(
+            {'serial_no' : self.serial_no.value.ljust(15, ' ')}, query)[0][0]:
+            npyscreen.notify_confirm("Serial no already exists in database.", 
+                                     title="Serial no error",
+                                     form_color='STANDOUT', wrap=True,
                                      wide=False, editw=1)
             return False
-
-        # check if serial_no is in existing database
-        query = "SELECT count(SERIAL_NO) FROM vehicle where SERIAL_NO = :serial_no"
-        if self.parentApp.db.query({'serial_no' : self.serial_no.value.ljust(15, ' ')}, 
-                query)[0][0]:
-            npyscreen.notify_confirm("Serial no already exists in database.", 
-                title="Serial no error", form_color='STANDOUT', wrap=True, wide=False, editw=1)
-            return False
         
-        # ensure that the provided vehicle type id exists in the database
+        # Ensure that the provided vehicle type id exists in the database
         if not self.type_id.value == '':
-            query = "SELECT count(type_id) FROM vehicle_type where type_id = :type_id"
-            if self.parentApp.db.query({'type_id' : int(self.type_id.value)}, 
-                query)[0][0] == 0:
-                npyscreen.notify_confirm("Vehicle Type ID %s does not exist in database."%self.type_id.value, title="Type ID Error", form_color='STANDOUT', wrap=True, wide=False, editw=1)
+            query = """SELECT count(type_id) FROM vehicle_type where type_id =
+                       :type_id"""
+            if self.parentApp.db.query(
+                {'type_id' : int(self.type_id.value)}, query)[0][0] == 0:
+                npyscreen.notify_confirm(
+                    "Vehicle Type ID %s does not exist in database."%(
+                        self.type_id.value), title="Type ID Error",
+                        form_color='STANDOUT', wrap=True, wide=False, editw=1)
                 return False
 
-        # notify user that the first owner must be primary
+        # Notify user that the first owner must be primary
         if self.p_owner.values[0] == '':
             npyscreen.notify_confirm("Please enter a primary owner.", 
-                title="Error", form_color='STANDOUT', wrap=True, wide=False, editw=1)
+                                     title="Error", form_color='STANDOUT',
+                                     wrap=True, wide=False, editw=1)
             return False
         
-        # if we pass all of our conditions return true to the caller
-        # to indicate it can proceed with database entry
+        # If we pass all of our conditions return True to the caller to indicate
+        # it can proceed with database entry.
         return True
     
-    # method is called when the user presses the ok button on the new vehicle reg form
+    # Method is called when the user presses the ok button on the new vehicle
+    # registration form.
     def on_ok(self):
-        # ensure the user specifies a serial number 
+        # Ensure the user specifies a serial number 
         if not self.serial_no.value:
-            npyscreen.notify_confirm("Please fill in a Serial no.", title="Error", form_color='STANDOUT', wrap=True, wide=False, editw=1)
+            npyscreen.notify_confirm("Please fill in a Serial no.",
+                                     title="Error", form_color='STANDOUT',
+                                     wrap=True, wide=False, editw=1)
             self.editing = True
             return        
 
-        # call data validation method
+        # Call data validation method
         if not self.validate_entries():
             self.editing = True
             return
 
-        # attempt to make the database entry catch any errors that occur
-        # and redirect the user back to the form
+        # Attempt to make the database entry catch any errors that occur and
+        # redirect the user back to the form.
         entry_dict = self.process_data()
         insert = self.prepare_statement()
         error = self.parentApp.db.insert(entry_dict, insert)
         if error:
-            # handle error, then return to form.
+            # Handle error, then return to form.
             self.editing = True
             npyscreen.notify_confirm(str(error), title="Status", 
-                form_color='STANDOUT', wrap=True, wide=False, editw=1)
+                                     form_color='STANDOUT', wrap=True,
+                                     wide=False, editw=1)
             return
 
-        # send primary owner data to db
+        # Send primary owner data to db
         values = {"owner_id"          :self.p_owner.values[0],
                   "vehicle_id"        :self.serial_no.value,
                   "is_primary_owner"  :'y'}
-        prepare = "INSERT INTO owner VALUES (:owner_id, :vehicle_id, :is_primary_owner)"
+        prepare = """INSERT INTO owner VALUES (:owner_id, :vehicle_id,
+                     :is_primary_owner)"""
         error = self.parentApp.db.insert(values, prepare)
         if error:
-            # handle error avoid main menu return
+            # Handle error avoid main menu return
             self.editing = True
-            npyscreen.notify_confirm(str(error), title="Error", form_color='STANDOUT', 
-                wrap=True, wide=False, editw=1)
+            npyscreen.notify_confirm(str(error), title="Error",
+                                     form_color='STANDOUT', wrap=True,
+                                     wide=False, editw=1)
             return
 
-
-        # send each secondary owner data to db
+        # Send each secondary owner data to db
         for owner in self.o_owners.values:
             values = {"owner_id"          :owner,
                       "vehicle_id"        :self.serial_no.value,
                       "is_primary_owner"  :'n'}
-            prepare = "INSERT INTO owner VALUES (:owner_id, :vehicle_id, :is_primary_owner)"
+            prepare = """INSERT INTO owner VALUES (:owner_id, :vehicle_id,
+                         :is_primary_owner)"""
             error = self.parentApp.db.insert(values, prepare)
             if error:
-                # handle error avoid main menu return
+                # Handle error avoid main menu return
                 self.editing = True
-                npyscreen.notify_confirm(str(error), title="Error", form_color='STANDOUT', 
-                    wrap=True, wide=False, editw=1)
+                npyscreen.notify_confirm(str(error), title="Error",
+                                         form_color='STANDOUT', wrap=True,
+                                         wide=False, editw=1)
                 return
         
-        # notify user that entry was successfull
-        npyscreen.notify_confirm("Success!", title="Status", form_color='STANDOUT', 
-            wrap=True, wide=False, editw=1)
+        # Notify user that entry was successful
+        npyscreen.notify_confirm("Success!", title="Status",
+                                 form_color='STANDOUT', wrap=True, wide=False,
+                                 editw=1)
 
         self.serial_no.value = ''
         self.maker.value = ''
@@ -178,8 +191,8 @@ class NewVehicleRegistration(npyscreen.ActionForm):
         self.o_owners.values = self.parentApp.NVR_other_owners
         self.p_owner.values = self.parentApp.NVR_primary_owner
 
-    # method called when the user selects the cancel button,
-    # clears all fields and returns them to the main menu
+    # Method called when the user selects the cancel button, clears all fields
+    # and returns them to the main menu.
     def on_cancel(self):
         self.serial_no.value = ''
         self.maker.value = ''
